7935e431c09fda7991493ea73beb9f17d8bfc667
ivadsiuls <150863674+ivadsiuls@users.noreply.github.com>
2024-04-01 17:06:44 -0300

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -17,6 +17,11 @@ namespace test
         {
             InitializeComponent();
         }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
 
         private void label1_Click(object sender, EventArgs e)
         {
